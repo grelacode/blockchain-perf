0368cfb71811b1283720b894d832478ed3b67498
grelacode <gonzalongrela@gmail.com>
2021-07-17 13:18:54 -0300

    initializing server

---
+++ src/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { INTERNAL_SERVER_ERROR, NOT_FOUND } = require('./constants/http_codes');
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods',
+        'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers',
+        'X-Requested-With,Content-Type,Accept,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = NOT_FOUND;
+    next(error);
+});
+
+app.use((error, req, res) => {
+    res.status(error.status || INTERNAL_SERVER_ERROR);
+    res.json({
+        error: {
+            message: error.message,
+        },
+    });
+});
+
+module.exports = app;
